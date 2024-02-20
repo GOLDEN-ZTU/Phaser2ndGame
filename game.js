129d457629f6ebfe76ac2032cbd7ce3864f2ab52
GOLDEN-ZTU <23b_gai@liceum.ztu.edu.ua>
2024-02-20 17:19:11 +0200

    Відцентровуємо задній фон

--- game.js
+++ game.js
@@ -44,13 +44,13 @@ function preload ()
 function create ()
 {
     
-    this.add.image(400, 300, 'sky');
+    this.add.image(980, 500, 'sky');
     
     
     platforms = this.physics.add.staticGroup();
 
     
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(400, 808, 'ground').setScale(2).refreshBody();
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
